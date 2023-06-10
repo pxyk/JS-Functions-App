c1c6fcd7522140e332b8128d4b22c45ca55f8fdd
Mert Aytemiz <101509856+pxyk@users.noreply.github.com>
2023-06-10 10:03:32 +0300

    Third Commit

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function faktoriyel(sayi){
 
 
 console.log(faktoriyel(sayi));
+
+
+//sayi = prompt("sayi giriniz : ")
+//function faktoriyel(sayi){
+//    var sonuc = 1;
+//    for(var i=2;i<=sayi;i++){
+//        sonuc*=i;
+//    }
+//    return sonuc;
+//}
+//console.log(faktoriyel(sayi));
