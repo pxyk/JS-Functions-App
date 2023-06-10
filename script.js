050a3916652613687cc250a7bf691e8401c1ca86
Mert Aytemiz <mertaytemizwork@gmail.com>
2023-06-10 09:57:44 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+sayi = prompt("sayi giriniz : ")
+function faktoriyel(sayi){
+
+    var sonuc = 1;
+
+    for(var i=2;i<=sayi;i++){
+        sonuc*=i;
+    }
+    return sonuc;
+}
+
+console.log(faktoriyel(sayi));
